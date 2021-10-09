4b7304994a13a74cf1882936ac40dec6e98bd5c8
Gonzalo <gonzalo.arilla@e-campus.uab.cat>
2021-10-09 17:25:43 +0200

    Minor changes in Main() to allow sleeps and run tests

--- src/Client.java
+++ src/Client.java
@@ -4,7 +4,19 @@ public class Client {
 
   }
 
-  public static void main(String[] args) {
-    System.out.println("Hello");
+
+  private static void testIntervalAndClock() throws InterruptedException {
+    //Gonzalo
+    Task task = new Task();
+    Interval interval = new Interval(task);
+    Thread.sleep(7000);
+    interval.finish();
+  }
+
+  public static void main(String[] args) throws InterruptedException {
+
+    testIntervalAndClock();
+
+
   }
 }
