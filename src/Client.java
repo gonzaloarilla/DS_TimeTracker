c6819d6e40c77e7d36925a191c425118e8c8008e
Gonzalo <gonzalo.arilla@e-campus.uab.cat>
2021-10-14 20:21:35 +0200

    Add new test + Changes in Task (mainly new start() method)

--- src/Client.java
+++ src/Client.java
@@ -13,6 +13,17 @@ public class Client {
     interval.finish();
   }
 
+  private static void testProjectWithTaskAndIntervals() throws InterruptedException {
+    //Gonzalo
+    Project project1 = new Project("DS"); //creem projecte1
+    Task task1 = new Task("Projecte DS"); //creem task1
+    project1.addNode(task1); //diem que projecte1 que conte la task1
+    task1.startTask();
+    Thread.sleep(7000);
+
+  }
+
+
   public static void main(String[] args) throws InterruptedException {
 
     testIntervalAndClock();
