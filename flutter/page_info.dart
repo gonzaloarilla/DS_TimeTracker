39c988b428bb0b791c084716d40472980d173882
Malfonso91 <malfonso621@gmail.com>
2022-01-04 21:00:49 +0100

    page information added

--- flutter/page_info.dart
+++ flutter/page_info.dart
@@ -1,32 +1,147 @@
-
 import 'package:flutter/material.dart';
-
+import 'package:time_tracker_flutter/tree.dart' as Tree hide getTree;
+// to avoid collision with an Interval class in another library
+import 'package:time_tracker_flutter/requests.dart';
 
 class PageInfo extends StatefulWidget {
-  // late final int id;
+  final int id; // final because StatefulWidget is immutable
 
-  late int parentId;
-  PageInfo();
+  PageInfo(this.id);
 
-  // @override
+  @override
   _PageInfoState createState() => _PageInfoState();
-
-
-
-
 }
 
 class _PageInfoState extends State<PageInfo> {
-  late int parentId;
+  late int id;
+  late Future<Tree.Tree> futureTree;
 
   @override
   void initState() {
     super.initState();
-  }
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-    );
+    id = widget.id;
+    futureTree = getTree(id);
   }
 
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<Tree.Tree>(
+      future: futureTree,
+      // this makes the tree of children, when available, go into snapshot.data
+      builder: (context, snapshot) {
+        // anonymous function
+          int numChildren = snapshot.data!.root.children.length;
+          return Scaffold(
+            appBar: AppBar(
+              title: Text(snapshot.data!.root.name + " information"),
+              actions: <Widget>[
+                IconButton(icon: Icon(Icons.home),
+                onPressed: () {
+                  Navigator.popUntil(context, ModalRoute.withName('/'));
+                  }
+                ),
+              ],
+            ),
+            body: Container(
+              margin: EdgeInsets.only(left: 40),
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Row(
+                      children: [
+                        Container(
+                            width: 120,
+                            child: Text('Start Date: ',
+                              style: new TextStyle(
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            )
+                        ),
+                        Text(snapshot.data!.root.initialDate.toString()),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Container(
+                            width: 120,
+                            child: Text('Final Date: ',
+                              style: new TextStyle(
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            )
+                        ),
+                        Text(snapshot.data!.root.lastDate.toString()),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Container(
+                            width: 120,
+                            child: Text('Duration: ',
+                              style: new TextStyle(
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            )
+                        ),
+                        Text(snapshot.data!.root.duration.toString()),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Container(
+                            width: 120,
+                            child: Text('Tags: ',
+                              style: new TextStyle(
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            )
+                        ),
+                        Text("getTags"),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Container(
+                            width: 120,
+                            child: Text('Description: ',
+                              style: new TextStyle(
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            )
+                        ),
+                        Text("getDescription"),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                            child: TextButton(
+                              style: TextButton.styleFrom(
+                                  textStyle: TextStyle(
+                                      fontSize: 20, fontWeight: FontWeight.bold)
+                              ),
+                              onPressed: () {
+                                // _printThings(id);
+                                // _getInfoTree(id);
+                              },
+                              child: const Text('Test'),
+                            )
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          );
+        }
+    );
+  }
 }
